93a3a3c034c7b0b30201a049f64fb336e6e6dcbd
thechiefpotatopeeler <thechiefpotatopeeler@gmail.com>
2024-03-22 10:07:04 +1300

    Began work on 3d

---
+++ README.md
@@ -0,0 +1,6 @@
+Hi
+
+Here's some helpful links:
+
+https://github.com/mgsx-dev/gdx-gltf
+https://libgdx.com/wiki/
--- core/src/com/chiefsource/unseenrealms/Main.java
+++ core/src/com/chiefsource/unseenrealms/Main.java
@@ -1,9 +1,12 @@
 package com.chiefsource.unseenrealms;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import net.mgsx.gltf.loaders.glb.GLBAssetLoader;
+import net.mgsx.gltf.scene3d.scene.SceneAsset;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -11,8 +14,9 @@ public class Main extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		AssetManager assMan = new AssetManager();
+		assMan.setLoader(SceneAsset.class, ".glb",new GLBAssetLoader());
+		assMan.load("src/resources/rockroom.glb",SceneAsset.class);
 	}
 
 	@Override
