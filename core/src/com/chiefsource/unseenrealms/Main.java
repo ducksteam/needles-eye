60abf02cf417b3267c598ee19117a3ff420f787e
thechiefpotatopeeler <thechiefpotatopeeler@gmail.com>
2024-03-22 10:36:45 +1300

    Decided that gltf/glb is wrong format, awaiting conversion

--- core/src/com/chiefsource/unseenrealms/Main.java
+++ core/src/com/chiefsource/unseenrealms/Main.java
@@ -2,28 +2,33 @@ package com.chiefsource.unseenrealms;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import net.mgsx.gltf.loaders.glb.GLBAssetLoader;
 import net.mgsx.gltf.scene3d.scene.SceneAsset;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
+	ModelBatch batch;
 	Texture img;
+	AssetManager assMan;
+	PerspectiveCamera camera;
 	
 	@Override
 	public void create () {
-		AssetManager assMan = new AssetManager();
-		assMan.setLoader(SceneAsset.class, ".glb",new GLBAssetLoader());
-		assMan.load("src/resources/rockroom.glb",SceneAsset.class);
+		batch = new ModelBatch();
+		camera = new PerspectiveCamera();
+		assMan = new AssetManager();
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.begin(camera);
+		batch.render();
 		batch.end();
 	}
 	
