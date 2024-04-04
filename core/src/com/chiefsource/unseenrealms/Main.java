73a33b9b348eea6925650a4e876c5ab613b9e7c6
thechiefpotatopeeler <thechiefpotatopeeler@gmail.com>
2024-04-04 14:51:46 +1300

    Built basic rendering system

--- core/src/com/chiefsource/unseenrealms/Main.java
+++ core/src/com/chiefsource/unseenrealms/Main.java
@@ -4,35 +4,58 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.chiefsource.unseenrealms.map.MapManager;
+
+import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
 	ModelBatch batch;
 	AssetManager assMan;
 	PerspectiveCamera camera;
 	MapManager mapMan;
+	Environment environment;
+	ArrayList<ModelInstance> modelInstances = new ArrayList<>();
 	
 	@Override
 	public void create () {
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
+		environment = new Environment();
 		batch = new ModelBatch();
 		camera = new PerspectiveCamera(80, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
 		assMan = new AssetManager();
 		mapMan = new MapManager();
+
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+		camera.position.set(5.0F,5.0F,5.0F);
+		camera.lookAt(new Vector3(0,0,0));
+		camera.update();
+
+
+		assMan.load("models/enemies/worm.g3dj", Model.class);
+		assMan.finishLoading();
+		modelInstances.add(new ModelInstance((Model) assMan.get("models/enemies/worm.g3dj")));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1, true);
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
 		batch.begin(camera);
-		//batch.render();
+		batch.render(modelInstances,environment);
 		batch.end();
+
+		//camera.update();
 	}
 	
 	@Override
