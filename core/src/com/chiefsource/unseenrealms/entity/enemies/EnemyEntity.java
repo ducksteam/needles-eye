ccd8f81532fb8a0df4ad14e5dea9ef1c8ea7712e
thechiefpotatopeeler <thechiefpotatopeeler@gmail.com>
2024-04-08 10:39:25 +1200

    Created enemy classes and enemy render list

---
+++ core/src/com/chiefsource/unseenrealms/entity/enemies/EnemyEntity.java
@@ -0,0 +1,10 @@
+package com.chiefsource.unseenrealms.entity.enemies;
+
+import com.badlogic.gdx.math.Vector3;
+import com.chiefsource.unseenrealms.entity.Entity;
+
+public abstract class EnemyEntity extends Entity {
+    public EnemyEntity( Vector3 position) {
+        super(position);
+    }
+}
