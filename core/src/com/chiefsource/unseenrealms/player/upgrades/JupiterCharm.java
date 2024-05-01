a0a451428a270b159125fd104f646ba3d568b564
SkySourced <58579786+SkySourced@users.noreply.github.com>
2024-05-01 15:02:42 +1200

    Added a second upgrade

---
+++ core/src/com/chiefsource/unseenrealms/player/upgrades/JupiterCharm.java
@@ -0,0 +1,25 @@
+package com.chiefsource.unseenrealms.player.upgrades;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.chiefsource.unseenrealms.Main;
+import com.chiefsource.unseenrealms.player.Upgrade;
+
+/**
+ * An upgrade that increases max life by 3 and decreases speed by 20%
+ */
+public class JupiterCharm extends Upgrade {
+
+    final int maxLifeIncrease = 3;
+    final float speedDecrease = 0.8f;
+
+    public JupiterCharm(String name, String description, Texture icon, Model model) {
+        super("Jupiter", "Life up, speed down", icon, model);
+    }
+
+    @Override
+    public void onPickup() {
+        Main.player.setSpeed(Main.player.getSpeed() * speedDecrease);
+        Main.player.setMaxHealth(Main.player.getMaxHealth() + maxLifeIncrease, true);
+    }
+}
