011b5ca5cf81ca98814045ef45d972a7925a2ee5
SkySourced <58579786+SkySourced@users.noreply.github.com>
2024-07-22 15:45:58 +1200

    Fixed SceneAsset ClassCastException

--- core/src/com/ducksteam/needleseye/entity/WallObject.java
+++ core/src/com/ducksteam/needleseye/entity/WallObject.java
@@ -1,9 +1,9 @@
 package com.ducksteam.needleseye.entity;
 
-import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
+import net.mgsx.gltf.scene3d.scene.SceneAsset;
 
 import static com.ducksteam.needleseye.Main.assMan;
 
@@ -11,8 +11,9 @@ public class WallObject extends Entity {
 
     public static String modelAddress = "models/rooms/door.gltf";
 
+
     public WallObject(Vector3 position, Quaternion rotation) {
-        super(position, rotation, new ModelInstance((Model) assMan.get("models/rooms/door.gltf")));
+        super(position, rotation, new ModelInstance(((SceneAsset) assMan.get("models/rooms/door.gltf")).scene.model));
     }
 
     @Override
