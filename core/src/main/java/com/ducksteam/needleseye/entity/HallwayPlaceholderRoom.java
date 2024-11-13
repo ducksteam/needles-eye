a21bc4158ce8d6a121233c95403be6be894b300c
SkySourced <58579786+SkySourced@users.noreply.github.com>
2024-11-13 21:17:00 +1300

    Added EnemyTagPosition record and importing into RoomTemplate

--- core/src/main/java/com/ducksteam/needleseye/entity/HallwayPlaceholderRoom.java
+++ core/src/main/java/com/ducksteam/needleseye/entity/HallwayPlaceholderRoom.java
@@ -4,11 +4,13 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.ducksteam.needleseye.map.RoomTemplate;
 
+import java.util.ArrayList;
+
 public class HallwayPlaceholderRoom extends RoomInstance {
     Vector2 associatedRoomCoords;
     RoomInstance associatedRoom;
 
-    public static final RoomTemplate HALLWAY_PLACEHOLDER_TEMPLATE = new RoomTemplate(RoomTemplate.RoomType.HALLWAY_PLACEHOLDER, 0, 0, false, null, null, new Vector3(0, 0, 0));
+    public static final RoomTemplate HALLWAY_PLACEHOLDER_TEMPLATE = new RoomTemplate(RoomTemplate.RoomType.HALLWAY_PLACEHOLDER, 0, 0, false, null, new Vector3(0, 0, 0), new ArrayList<>());
 
     public HallwayPlaceholderRoom(Vector2 position, RoomInstance associatedRoom) {
         super(HALLWAY_PLACEHOLDER_TEMPLATE, position, associatedRoom.rot);
