2b47ee5d697424784e6c3724dfad9ed0232fa008
Thechiefpotatopeeler <69015883+Thechiefpotatopeeler@users.noreply.github.com>
2024-12-11 16:29:31 +1300

    Fixed frustum culling

--- core/src/main/java/com/ducksteam/needleseye/map/MapManager.java
+++ core/src/main/java/com/ducksteam/needleseye/map/MapManager.java
@@ -14,6 +14,7 @@ import com.ducksteam.needleseye.entity.RoomInstance;
 import com.ducksteam.needleseye.entity.WallObject;
 import com.ducksteam.needleseye.entity.enemies.EnemyEntity;
 import com.ducksteam.needleseye.entity.enemies.EnemyTag;
+import com.ducksteam.needleseye.entity.enemies.OrbulonEnemy;
 
 import java.util.*;
 import java.util.function.Predicate;
