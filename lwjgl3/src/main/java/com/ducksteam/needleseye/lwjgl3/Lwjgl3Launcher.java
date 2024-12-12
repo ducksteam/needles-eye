ed7e88ed447a795630f08aa63d93a5997390a9cf
themoonboyx <themoonboyx@users.noreply.github.com>
2024-12-13 11:51:55 +1300

    Reverted resolution because weird stuff was happening

--- lwjgl3/src/main/java/com/ducksteam/needleseye/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/ducksteam/needleseye/lwjgl3/Lwjgl3Launcher.java
@@ -1,6 +1,5 @@
 package com.ducksteam.needleseye.lwjgl3;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.glutils.HdpiMode;
