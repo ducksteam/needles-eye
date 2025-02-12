a4edb14493b93c1e4d67152e46b9204dfdf14a78
SkySourced <58579786+SkySourced@users.noreply.github.com>
2025-02-12 18:20:50 +1300

    Fix debug drawer
    Fixes #40

--- core/src/main/java/com/ducksteam/needleseye/player/Player.java
+++ core/src/main/java/com/ducksteam/needleseye/player/Player.java
@@ -136,7 +136,8 @@ public class Player extends Entity implements IHasHealth {
         dynamicsWorld.dispose();
         dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, constraintSolver, collisionConfig);
         dynamicsWorld.setGravity(new Vector3(0, -14f, 0));
-
+        dynamicsWorld.setDebugDrawer(debugDrawer);
+        
         // build the player's model
         sceneModel = new Scene(new Model());
         setScene(sceneModel);
